c866945fa00d812ff9c20a078bcc5aff4993ccca
Juana Rincon <juana.876@hotmail.com>
2024-05-05 22:05:44 -0500

    initial commit Se agrego un fetchGet
    Signed-off-by: Juana Rincon <juana.876@hotmail.com>

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import { useState } from 'react'
+import './App.css'
+//import FetchGet from './FetchGet'
+import Formulario from './Formulario';
+
+
+function App() {
+  const [showSuperheroes, setShowSuperheroes] = useState(false)
+  const [createSuperheroes, setCreateSuperheroes] = useState(false) 
+  const [modifySuperheroes, setModifySuperheroes] = useState(false)
+  const [deleteSuperheroes, setDeleteSuperheroes] = useState(false)
+  
+  const getButtonText = (showState, text1, text2) => {
+    return showState ? `${text2}` : `${text1}`;
+};
+  const toggleShowSuperheroes = () => {
+        setShowSuperheroes(!showSuperheroes);
+    };
+  const toggleCreateSuperheroes = () => {
+        setCreateSuperheroes(!createSuperheroes);
+    };
+  const toggleModifySuperheroes = () => {
+          setModifySuperheroes(!modifySuperheroes);
+    };
+  const toggleDeleteSuperheroes = () => {
+          setDeleteSuperheroes(!deleteSuperheroes);
+    };
+
+
+  const getButtonClassName = (showState) => {
+    return showState ? 'boton-menu presionado' : 'boton-menu';
+};
+
+
+
+
+  return (  
+  <>
+    <header>
+      <div className="container">
+          <span style={{ '--l': "'S'" }}>S</span>
+          <span style={{ '--l': "'U'" }}>U</span>
+          <span style={{ '--l': "'P'" }}>P</span>
+          <span style={{ '--l': "'E'" }}>E</span>
+          <span style={{ '--l': "'R'" }}>R</span>
+          <span style={{ '--l': "'H'" }}>H</span>
+          <span style={{ '--l': "'E'" }}>E</span>
+          <span style={{ '--l': "'R'" }}>R</span>
+          <span style={{ '--l': "'O'" }}>O</span>
+          <span style={{ '--l': "'E'" }}>E</span>
+          <span style={{ '--l': "'S'" }}>S</span>
+        </div>
+    </header>
+    <aside className='menu'>
+          <button className={getButtonClassName(showSuperheroes)} onClick={toggleShowSuperheroes}>
+            {getButtonText(showSuperheroes, 'Mostrar Superheroes','Mostrando Superheroes' )}
+          </button>
+          <button className={getButtonClassName(createSuperheroes)} onClick={toggleCreateSuperheroes}>
+            {getButtonText(createSuperheroes, 'Crear Superheroes', 'Creando Superheroes')}
+          </button>
+          <button className={getButtonClassName(modifySuperheroes)} onClick={toggleModifySuperheroes}>
+            {getButtonText(modifySuperheroes, 'Modificar Superheroes', 'Modificando Superheroes')}
+          </button>
+          <button className={getButtonClassName(deleteSuperheroes)} onClick={toggleDeleteSuperheroes}>
+            {getButtonText(deleteSuperheroes, 'Eliminar Superheroes', 'Eliminando Superheroes')}
+          </button>
+      
+    </aside>
+    {createSuperheroes && <Formulario />}
+
+
+  
+  
+  </>
+
+
+  );
+}
+
+export default App
---
+++ src/Formulario.jsx
@@ -0,0 +1,117 @@
+import './Formulario.css'
+import { useForm } from "react-hook-form";
+
+
+function Formulario() {
+    const { register , handleSubmit , formState: {errors}} = useForm()
+
+    const onSubmit = handleSubmit((data)=> {
+        console.log(data)})
+  return (
+    <section className='form-register'>
+    <form onSubmit={onSubmit}>
+      <label>
+        <h1>Name:</h1>
+        <input className= "controls" type="text" name="name" 
+        {...register("name" , {
+            required: {
+                value:true,
+                message: "Name es requerido"},
+            minLength:{
+                value:2,
+                message: "Name  debe ser mayor a 2 caracteres"},
+            maxLength: {
+                value:20,
+                message: "Name no debe ser mayor a 20 caracteres"},
+        })}/>
+        {errors.name && <p className='errores'>{errors.name.message}</p>}
+
+
+
+      </label>
+
+      <label>
+        <h1>Nickname:</h1>
+        <input  className= "controls"type="text" name="nickname"
+         {...register("nickname" , {
+            required: {
+                value:true,
+                message: "Nickname es requerido"},
+            minLength:{
+                value:2,
+                message: "Nickname  debe ser mayor a 2 caracteres"},
+            maxLength: {
+                value:20,
+                message: "Nickname no debe ser mayor a 20 caracteres"},
+        })}/>
+        {errors.nickname && <p className='errores'>{errors.nickname.message}</p>}
+
+
+      </label>
+      <label>
+        <h1>Age:</h1>
+        <input className= "controls" type="number" name="age" 
+        {...register("age",
+            {required: true,
+                pattern: {
+                    value: /^[0-9]+$/,
+                    message: "Por favor, introduce un número entero válido."
+                  }
+            }
+        )}/>
+        {errors.age && errors.age.type === "required" && <p className='errores'>Age es requerido</p>}
+
+        {errors.age && <p className='errores'>{errors.age.message}</p>}
+
+      </label>
+      <label>
+        <h1>Color Suit:</h1>
+        <input className= "controls" type="text" name="colorSuit" 
+        {...register("colorSuit",
+            {required: {
+                value:true,
+                message: "ColorSuit es requerido"},
+            minLength:{
+                value:2,
+                message: "ColorSuit  debe ser mayor a 2 caracteres"},
+            maxLength: {
+                value:20,
+                message: "ColorSuit no debe ser mayor a 20 caracteres"},
+        })}/>
+        {errors.colorSuit && <p className='errores'>{errors.colorSuit.message}</p>}
+
+      </label>
+      
+      <label>
+        <h1>Description:</h1>
+        <input className= "controls"  type="text" name="description" 
+        {...register("description",
+            {required: {
+                value:true,
+                message: "Description es requerido"},
+            minLength:{
+                value:2,
+                message: "Description  debe ser mayor a 2 caracteres"},
+            maxLength: {
+                value:20,
+                message: "Description no debe ser mayor a 20 caracteres"},
+        })}/>
+        {errors.description && <p className='errores'>{errors.description.message}</p>}
+
+      </label>
+      
+
+      <label htmlFor='file'>
+        <h1>Foto del superheroe:</h1>
+        <input  type='file'  
+        {...register("file")}/>
+      </label>
+
+      <input type="submit" value="Submit" />
+    </form>
+    </section>
+
+  );
+}
+
+export default Formulario;
